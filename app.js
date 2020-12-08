0972d26003a2110cb4197ee1c2e418af82bbd41f
Denis Koliban <deniskoliban@gmail.com>
2020-12-08 14:02:59 +0200

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+const express = require('express')
+const app = express();
+app.get('/api', (req, res) => {
+    res.send({ message: 'Welcome to express-app!' });
+});
+
+const port = process.env.port || 3333;
+const server = app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on('error', console.error);
